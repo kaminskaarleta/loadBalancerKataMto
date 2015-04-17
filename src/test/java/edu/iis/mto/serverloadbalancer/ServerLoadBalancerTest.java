fb261c4f64ddbf9d3823436451dde1a2280fce5b
kaminskaarleta <180195@edu.p.lodz.pl>
2015-04-17 11:12:09 +0200

    test 1 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -12,5 +12,10 @@ public class ServerLoadBalancerTest {
 		assertThat(true, equalTo(true));
 	}
 
-
+	@Test
+	public void balancingServer_noVm_ServerStaysEmpty(){
+		Server server = a(server().withCapacity(1));
+		balance(listOfServerWith(server), emptyListOfVms());
+		assertThat(server, hasLoadPercentageOf(0.0d));	
+	}
 }
